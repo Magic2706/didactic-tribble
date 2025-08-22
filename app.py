287686edae3e61ddd97d3eedf5eef193522f6826
Magic2706 <thefunmakesyoudrivecrazy@gmail.com>
2025-08-22 19:29:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ import plotly.express as px
 # ---------------------------
 # Google Sheets Authentication
 # ---------------------------
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
-client = gspread.authorize(creds)
 
-# Open your Google Sheet by name
-sheet = client.open("Cigarette Tracker").sheet1
+from google.oauth2.service_account import Credentials
+
+
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+
+creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
+client = gspread.authorize(creds)
 
 # ---------------------------
 # Helper Functions
