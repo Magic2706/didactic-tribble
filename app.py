269fa049489f876054aca12be20b0b050410e620
Magic2706 <thefunmakesyoudrivecrazy@gmail.com>
2025-08-22 19:42:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ import plotly.express as px
 # ---------------------------
 # Authenticate first (GLOBAL)
 # ---------------------------
+
+from google.oauth2.service_account import Credentials
+
+
 scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+
 creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
 client = gspread.authorize(creds)
-
-# Open your sheet globally
 sheet = client.open("Cigarette Tracker").sheet1
-
 # ---------------------------
 # Helper functions
 # ---------------------------
