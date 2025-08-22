69b468f3cfd7df12db0203d0f052858705726541
Magic2706 <thefunmakesyoudrivecrazy@gmail.com>
2025-08-22 19:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from google.oauth2.service_account import Credentials
 scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
 creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
 client = gspread.authorize(creds)
-sheet = client.open("Cigarette Tracker").sheet1
+sheet = client.open("Cigarette").sheet1
 print(sheet.row_values(1))
 # ---------------------------
 # Helper Functions
